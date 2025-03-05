adca79fa04a1f5a5521da7097cb876f639989d5c
sudo-shaka <shaka.x@yale.edu>
2025-03-04 20:21:53 -0500

    better test example

--- test.py
+++ test.py
@@ -6,22 +6,22 @@ import plot
 from matplotlib import pyplot as plt
 from progressbar import progressbar
 
-c = clDPM.Cell3D([0]*3,1.0,1.3) #input is [starting_point],CalA, radius
-c2 = clDPM.Cell3D([0]*3,1.0,1.4)
+c = clDPM.Cell3D([0]*3,1.05,1.1) #input is [starting_point],CalA, radius
+c2 = clDPM.Cell3D([0]*3,1.05,1.4)
 c.Ka = 1.5
 c.Kv = 0.6
-c.Ks = 2.5
+c.Ks = 3.5
 c2.Ka = 1.5
 c2.Kv = 0.6
-c2.Ks = 2.5
+c2.Ks = 3.5
 
-T = clDPM.Tissue3D([c2,c]*50,0.25) #input is: list of cell, and packing fraction (3D)
+T = clDPM.Tissue3D([c2,c]*21,0.35) #input is: list of cell, and packing fraction (3D)
 T.Kre = 10.0
 T.Disperse2D()
 
 Faces = T.Cells[0].GetFaces()
-nsteps = 200
-nout = 50
+nsteps = 50
+nout = 200
 for i in progressbar(range(nout)):
   plot.PlotTissue2D(T)
   plt.savefig('/tmp/test_' + str(i) + '.png')
