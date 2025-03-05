4041551a7fd868eebe41225ce08d837606381b19
sudo-shaka <68864205+sudo-shaka@users.noreply.github.com>
2025-03-05 13:35:06 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-#bear -- g++ -std=c++17 -o OPENGL_DPM3D test3D.cpp src/cell.cpp src/Tissue3D.cpp src/readKernel.cpp -lOpenCL -O3 -g -Wall -I include/
+bear -- g++ -std=c++17 -o OPENGL_DPM3D test3D.cpp src/cell.cpp src/Tissue3D.cpp src/readKernel.cpp -lOpenCL -O3 -g -Wall -I include/
 bear -- g++ -std=c++17 -o OPENGL_DPM2D test2D.cpp src/cell.cpp src/Tissue2D.cpp src/readKernel.cpp -lOpenCL -O3 -g -Wall -I include/
