bda6568f2adba9e36295e708177ac5ba426bcd10
sudo-shaka <shaka.x@yale.edu>
2025-03-06 18:58:42 -0500

    better example

--- test3D.py
+++ test3D.py
@@ -10,18 +10,18 @@ c = clDPM.Cell3D([0]*3,1.05,1.1) #input is [starting_point],CalA, radius
 c2 = clDPM.Cell3D([0]*3,1.05,1.4)
 c.Ka = 1.5
 c.Kv = 0.6
-c.Ks = 10.0
+c.Ks = 4.0
 c2.Ka = 1.5
 c2.Kv = 0.6
-c2.Ks = 10.0
+c2.Ks = 4.0
 
 T = clDPM.Tissue3D([c2,c]*50,0.3) #input is: list of cell, and packing fraction (3D)
 T.Kre = 10.0
 T.Disperse2D()
 
 Faces = T.Cells[0].GetFaces()
-nsteps = 10
-nout = 200
+nsteps = 100
+nout = 10
 for i in progressbar(range(nout)):
   plot.Plot3DTissue2D(T)
   plt.savefig('/tmp/test_' + str(i) + '.png')
