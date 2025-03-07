f5b8c6a748fd7ac75903f816ba704eb0ae90a222
sudo-shaka <shaka.x@yale.edu>
2025-03-06 19:39:35 -0500

    better example

--- test3D.py
+++ test3D.py
@@ -8,23 +8,24 @@ from progressbar import progressbar
 
 c = clDPM.Cell3D([0]*3,1.05,1.1) #input is [starting_point],CalA, radius
 c2 = clDPM.Cell3D([0]*3,1.05,1.4)
-c.Ka = 1.5
-c.Kv = 0.6
-c.Ks = 4.0
-c2.Ka = 1.5
-c2.Kv = 0.6
-c2.Ks = 4.0
+c.Ka = 0.6
+c.Kv = 1.5
+c.Ks = 5.0
+c2.Ka = 0.6
+c2.Kv = 1.5
+c2.Ks = 5.0
 
-T = clDPM.Tissue3D([c2,c]*50,0.3) #input is: list of cell, and packing fraction (3D)
+T = clDPM.Tissue3D([c2,c]*21,0.35) #input is: list of cell, and packing fraction (3D)
 T.Kre = 10.0
 T.Disperse2D()
 
 Faces = T.Cells[0].GetFaces()
-nsteps = 100
+nsteps = 500
 nout = 1
 for i in range(nout):
   print("Starting 3D simulation with "+str(T.NCELLS)+" particles for "+str(nsteps)+" timesteps...")
   T.CLEulerUpdate(nsteps, 0.001)
+  print("Plotting figure...")
   plot.Plot3DTissue2D(T)
   filename = "test.png"
   plt.savefig(filename)
